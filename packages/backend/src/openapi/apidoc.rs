fe47ff4445b446ad4a8001b7803f12b5bdbe4af0
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-01-12 17:35:31 +0700

    new api documentation for signin route

--- packages/backend/src/openapi/apidoc.rs
+++ packages/backend/src/openapi/apidoc.rs
@@ -4,11 +4,14 @@ use crate::openapi::security_addon::SecurityAddon;
 #[openapi(
     paths(
         crate::routes::hello::handler,
+        crate::routes::auth::signin::handler,
     ),
     components(
         schemas(
             crate::errors::FormattedErrorResponse,
-            crate::constants::GetServerInformationResponse
+            crate::constants::GetServerInformationResponse,
+            crate::routes::auth::signin::SigninBody,
+            crate::constants::DefaultSuccessResponse,
         )
     ),
     modifiers(&SecurityAddon)
