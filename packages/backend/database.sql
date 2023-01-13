d94b6ec1666276c375902692a505691d477bd2fe
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-01-13 17:13:55 +0700

    redefined database schema with new autogen table references in rust

--- packages/backend/database.sql
+++ packages/backend/database.sql
@@ -13,31 +13,101 @@ create type t_user_role as enum ('user', 'student', 'professor');
 drop table faculties cascade;
 drop table curriculums cascade;
 drop table majors cascade;
-drop table major_credit_specifications cascade;
-drop table major_subjects cascade;
 drop table academic_years cascade;
 drop table semesters cascade;
 drop table buildings cascade;
 drop table rooms cascade;
-drop table students cascade;
+drop table users cascade;
+drop table user_sessions cascade;
+drop table grading_criterias cascade;
+drop table grading_criteria_grades cascade;
+drop table subjects cascade;
 drop table subject_schedules cascade;
-drop table opening_subjects_in_semesters cascade;
+drop table students cascade;
+drop table student_names cascade;
+drop table professors cascade;
+drop table professor_names cascade;
+drop table major_credit_specifications cascade;
+drop table major_subjects cascade;
+drop table opening_subjects_in_semester_schedules cascade;
 drop table opening_subjects_in_semester_professors cascade;
-drop table opening_subjects_in_semester_subject_capacities;
+drop table opening_subjects_in_semester_subject_descriptions cascade;
 drop table opening_subjects_in_semester_eligible_majors cascade;
 drop table opening_subjects_in_semester_additional_eligible_students cascade;
 drop table student_enrollments cascade;
+drop table student_transactions cascade;
+drop table student_subject_comments cascade;
 drop table student_assignments cascade;
 drop table student_scores cascade;
-drop table professors cascade;
-drop table users cascade;
-drop table user_sessions cascade;
 
 -- available faculties in the university.
 create table faculties (
     faculty_id text not null unique,
     faculty_name text not null,
+    faculty_created_timestamp timestamptz not null default now(),
     primary key (faculty_id)
+);
+
+-- available curriculums for studying in the university, such as:
+-- - normal bachelor
+-- - special bachelor (saturdays and sundays)
+-- - master degree
+--
+-- this is directly tied to the faculties.
+create table curriculums (
+    curriculum_id text not null unique,
+    faculty_id text not null,
+    curriculum_name text not null,
+    curriculum_created_timestamp timestamptz not null default now(),
+    primary key (curriculum_id),
+    foreign key (faculty_id) references faculties(faculty_id)
+);
+
+-- -- majors in the faculty that you could take in the curriculum.
+create table majors (
+    major_id text not null unique,
+    curriculum_id text not null,
+    major_name text not null,
+    major_created_timestamp timestamptz not null default now(),
+    primary key (major_id),
+    foreign key (curriculum_id) references curriculums(curriculum_id)
+);
+
+-- years in the academic year
+create table academic_years (
+    academic_year_id text not null unique,
+    academic_year_gregorian_year int not null unique default date_part('year', now())::int,
+    academic_year_start_timestamp timestamptz not null default now(),
+    academic_year_end_timestamp timestamptz not null default now(),
+    primary key (academic_year_id)
+);
+
+-- available semesters in the year
+create table semesters (
+    semester_id text not null unique,
+    academic_year_id text not null,
+    semester_start_timestamp timestamptz not null default now(),
+    semester_end_timestamp timestamptz not null default now(),
+    primary key (semester_id),
+    foreign key (academic_year_id) references academic_years(academic_year_id)
+);
+
+-- buildings in the uni
+create table buildings (
+    building_id text not null unique,
+    building_name text not null,
+    building_cordinates point not null,
+    primary key (building_id)
+);
+
+-- rooms in the buildings
+create table rooms (
+    room_id text not null unique,
+    building_id text not null,
+    room_name text not null,
+    room_capacity int not null default 0,
+    primary key (room_id),
+    foreign key (building_id) references buildings(building_id)
 );
 
 create table users (
@@ -53,8 +123,45 @@ create table user_sessions (
     user_session_id text not null unique,
     user_session_user_id text not null unique,
     user_session_refresh_token text not null,
-    primary key user_session_id,
-    foreign key user_session_user_id references users(user_id)
+    primary key (user_session_id ),
+    foreign key (user_session_user_id) references users(user_id)
+);
+
+create table grading_criterias (
+    grading_criteria_id text not null unique,
+    user_id text not null,
+    grading_criteria_name text not null,
+    grading_criteria_description text not null,
+    grading_created_timestamp timestamptz not null,
+    primary key (grading_criteria_id),
+    foreign key (user_id) references users(user_id)
+);
+
+create table grading_criteria_grades (
+    grading_criteria_grade_id text not null unique,
+    grading_criteria_id text not null,
+    grading_criteria_grade_alphabet text not null,
+    grading_criteria_grade_minimum_score int not null
+);
+
+-- subjects opened for studying in the university
+create table subjects (
+    subject_id text not null unique,
+    subject_name text not null,
+    subject_credit int not null,
+    subject_type text not null,
+    primary key (subject_id)
+);
+
+-- one subject could span many hours during the week so this is required
+create table subject_schedules (
+    subject_schedule_id text not null unique,
+    subject_id text not null,
+    subject_schedule_day_of_week t_day_of_week not null,
+    subject_schedule_start_time_of_day time not null,
+    subject_schedule_end_time_of_day time not null,
+    primary key (subject_schedule_id),
+    foreign key (subject_id) references subjects(subject_id)
 );
 
 -- students data
@@ -105,35 +212,12 @@ create table professor_names (
     foreign key (professor_id) references professors(professor_id)
 );
 
--- available curriculums for studying in the university, such as:
--- - normal bachelor
--- - special bachelor (saturdays and sundays)
--- - master degree
---
--- this is directly tied to the faculties.
-create table curriculums (
-    curriculum_id text not null unique,
-    faculty_id text not null,
-    curriculum_name text not null,
-    curriculum_created_timestamp timestamptz not null default now(),
-    primary key (curriculum_id),
-    foreign key (faculty_id) references faculties(faculty_id)
-);
-
--- majors in the faculty that you could take in the curriculum.
-create table majors (
-    major_id text not null unique,
-    curriculum_id text not null,
-    major_name text not null,
-    primary key (major_id),
-    foreign key (curriculum_id) references curriculums(curriculum_id)
-);
-
 -- credit specifications for a major, this is how many credits you have to take
 -- so that you can graduate.
 create table major_credit_specifications (
     major_credit_specification_id text not null unique,
     major_id text not null,
+    major_credit_specification_name text not null,
     major_credit_specification_minimum_credit int not null,
     primary key (major_credit_specification_id),
     foreign key (major_id) references majors(major_id)
@@ -142,75 +226,15 @@ create table major_credit_specifications (
 -- subjects in the major, this is grouped by the credit specification as a group
 -- of subjects that you have to take.
 create table major_subjects (
-    major_subject_id text not null unique,
-    major_credit_specification_id text not null,
-    subject_id text not null,
-    primary key (major_subject_id),
-    foreign key (major_credit_specification_id) references major_credit_specifications(major_credit_specification_id),
-    foreign key (subject_id) references subjects(subject_id)
-);
-
--- years in the academic year
-create table academic_years (
-    academic_year_id text not null unique,
-    academic_year_gregorian_year text not null unique default date_part('year', now())::text,
-    academic_year_start_timestamp timestamptz not null default now(),
-    academic_year_end_timestamp timestamptz not null default now(),
-    primary key (academic_year_id)
-);
-
--- available semesters in the year
-create table semesters (
-    semester_id text not null unique,
-    academic_year_id text not null,
-    semester_start_timestamp timestamptz not null default now(),
-    semester_end_timestamp timestamptz not null default now(),
-    primary key (semester_id),
-    foreign key (academic_year_id) references academic_years(academic_year_id)
-);
-
--- buildings in the uni
-create table buildings (
-    building_id text not null unique,
-    building_name text not null,
-    building_cordinates point not null,
-    primary key (building_id)
-);
-
--- rooms in the buildings
-create table rooms (
-    room_id text not null unique,
-    building_id text not null,
-    room_name text not null,
-    room_capacity int not null default 0,
-    primary key (room_id),
-    foreign key (building_id) references buildings(building_id)
-);
-
--- subjects opened for studying in the university
-create table subjects (
-    subject_id text not null unique,
-    subject_name text not null,
-    subject_credit int not null,
-    subject_type text not null,
-    primary key (subject_id)
-);
-
--- one subject could span many hours during the week so this is required
-create table subject_schedules (
-    subject_schedule_id text not null unique,
-    subject_id text not null,
-    subject_schedule_day_of_week t_day_of_week not null,
-    subject_schedule_start_time_of_day time not null,
-    subject_schedule_end_time_of_day time not null,
-    primary key (subject_schedule_id),
-    foreign key (subject_id) references subjects(subject_id)
+    major_credit_specification_id text not null references major_credit_specifications(major_credit_specification_id),
+    subject_id text not null references subjects(subject_id),
+    primary key (major_subject_id, subject_id)
 );
 
 -- which subjects are opened for studying in each semester, this only stores the schedule
 -- and the room in which each schedule is stored, these can be tracked back to a subject
 -- and get the list of subjects in which they are open in the semester.
-create table opening_subjects_in_semesters (
+create table opening_subjects_in_semester_schedules (
     semester_id text not null references semesters(semester_id),
     subject_schedule_id text not null references subject_schedules(subject_schedule_id),
     room_id text not null references rooms(room_id),
@@ -226,10 +250,11 @@ create table opening_subjects_in_semester_professors (
 );
 
 -- how many students a subject is accepting in each semester and which students can enrol in the class
-create table opening_subjects_in_semester_subject_capacities (
+create table opening_subjects_in_semester_subject_descriptions (
     semester_id text not null references semesters(semester_id),
     subject_id text not null references subjects(subject_id),
     subject_capacity int not null,
+    is_grade_released boolean not null default false,
     primary key (semester_id, subject_id)
 );
 
@@ -255,11 +280,30 @@ create table student_enrollments (
     primary key (semester_id, subject_id, student_id)
 );
 
+create table student_transactions (
+    semester_id text not null references semesters(semester_id),
+    student_id text not null references students(student_id),
+    student_transaction_id text not null unique,
+    student_transaction_is_transaction_successful boolean not null default false,
+    student_transaction_price real not null,
+    student_transaction_created_timestamp timestamptz not null default now(),
+    student_transaction_finished_timestamp timestamptz,
+    primary key (semester_id, student_id)
+);
+
+create table student_subject_comments (
+    semester_id text not null references semesters(semester_id),
+    subject_id text not null references subjects(subject_id),
+    student_id text not null references students(student_id),
+    student_comment text not null,
+    primary key (semester_id, subject_id, student_id)
+);
+
 create table student_assignments (
     student_assignment_id text not null unique,
     student_assignment_name text not null,
-    subject_id text not null,
-    semester_id text not null,
+    subject_id text not null references subjects(subject_id),
+    semester_id text not null references semesters(semester_id),
     student_assignment_full_score real not null,
     primary key (student_assignment_id)
 );
--- packages/backend/src/database.rs
+++ packages/backend/src/database.rs
@@ -1,4 +1,5 @@
-enum DayOfWeek {
+#[derive(ger_from_row::FromRow)]
+pub enum DayOfWeek {
     Sunday,
     Monday,
     Tuesday,
@@ -8,86 +9,24 @@ enum DayOfWeek {
     Saturday,
 }
 
+#[derive(ger_from_row::FromRow)]
 pub enum Role {
     User,
     Student,
     Professor,
 }
 
-pub struct Students {
-    pub student_id: String,
-    pub student_representative_id: String,
-    pub student_profile_image_path: String,
-    pub student_nid: String,
-    pub student_previous_school_name: String,
-    pub student_previous_school_gpa: f64,
-    pub professor_id: String,
-    pub first_academic_year_id: String,
-}
-
-pub struct AcademicYears {
-    pub academic_year_id: String,
-    pub academic_year_gregorian_year: String,
-    pub curriculum_id: String,
-    pub academic_year_start_timestamp: time::OffsetDateTime,
-    pub academic_year_end_timestamp: time::OffsetDateTime,
-}
-
-pub struct Semesters {
-    pub semester_id: String,
-    pub academic_year_id: String,
-    pub semester_start_timestamp: time::OffsetDateTime,
-    pub semester_end_timestamp: time::OffsetDateTime,
-}
-
-pub struct Faculties {
-    pub faculty_id: String,
-    pub faculty_name: String,
-}
-
-pub struct Users {
-    pub user_id: String,
-    pub user_username: String,
-    pub user_email: String,
-    pub user_password: String,
-    pub user_role: what,
-}
-
-pub struct Majors {
-    pub major_id: String,
-    pub curriculum_id: String,
-    pub major_name: String,
-}
-
-pub struct MajorCreditSpecifications {
-    pub major_credit_specification_id: String,
-    pub major_id: String,
-    pub major_credit_specification_minimum_credit: i32,
-}
-
+#[derive(ger_from_row::FromRow)]
 pub struct SubjectSchedules {
     pub subject_schedule_id: String,
     pub subject_id: String,
+    #[fromrow(num)]
     pub subject_schedule_day_of_week: DayOfWeek,
     pub subject_schedule_start_time_of_day: time::Time,
     pub subject_schedule_end_time_of_day: time::Time,
 }
 
-pub struct OpeningSubjectsInSemesterEligibleMajors {
-    pub semester_id: String,
-    pub subject_id: String,
-    pub major_id: String,
-    pub academic_year_id: String,
-}
-
-pub struct StudentAssignments {
-    pub student_assignment_id: String,
-    pub student_assignment_name: String,
-    pub subject_id: String,
-    pub semester_id: String,
-    pub student_assignment_full_score: f64,
-}
-
+#[derive(ger_from_row::FromRow)]
 pub struct StudentNames {
     pub student_name_id: String,
     pub student_id: String,
@@ -97,11 +36,74 @@ pub struct StudentNames {
     pub student_last_name: String,
 }
 
+#[derive(ger_from_row::FromRow)]
+pub struct MajorSubjects {
+    pub major_credit_specification_id: String,
+    pub subject_id: String,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct StudentAssignments {
+    pub student_assignment_id: String,
+    pub student_assignment_name: String,
+    pub subject_id: String,
+    pub semester_id: String,
+    pub student_assignment_full_score: f64,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Buildings {
+    pub building_id: String,
+    pub building_name: String,
+    pub building_cordinates: geo_types::Point<f64>,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct StudentSubjectComments {
+    pub semester_id: String,
+    pub subject_id: String,
+    pub student_id: String,
+    pub student_comment: String,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Semesters {
+    pub semester_id: String,
+    pub academic_year_id: String,
+    pub semester_start_timestamp: time::OffsetDateTime,
+    pub semester_end_timestamp: time::OffsetDateTime,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Rooms {
+    pub room_id: String,
+    pub building_id: String,
+    pub room_name: String,
+    pub room_capacity: i32,
+}
+
+#[derive(ger_from_row::FromRow)]
 pub struct Professors {
     pub professor_id: String,
     pub professor_profile_image_path: String,
 }
 
+#[derive(ger_from_row::FromRow)]
+pub struct OpeningSubjectsInSemesterSubjectDescriptions {
+    pub semester_id: String,
+    pub subject_id: String,
+    pub subject_capacity: i32,
+    pub is_grade_released: bool,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct StudentEnrollments {
+    pub semester_id: String,
+    pub subject_id: String,
+    pub student_id: String,
+}
+
+#[derive(ger_from_row::FromRow)]
 pub struct Curriculums {
     pub curriculum_id: String,
     pub faculty_id: String,
@@ -109,39 +111,31 @@ pub struct Curriculums {
     pub curriculum_created_timestamp: time::OffsetDateTime,
 }
 
-pub struct Buildings {
-    pub building_id: String,
-    pub building_name: String,
-    pub building_cordinates: geo_types::Point<f64>,
+#[derive(ger_from_row::FromRow)]
+pub struct UserSessions {
+    pub user_session_id: String,
+    pub user_session_user_id: String,
+    pub user_session_refresh_token: String,
 }
 
-pub struct Subjects {
-    pub subject_id: String,
-    pub subject_name: String,
-    pub subject_credit: i32,
-    pub subject_type: String,
+#[derive(ger_from_row::FromRow)]
+pub struct GradingCriteriaGrades {
+    pub grading_criteria_grade_id: String,
+    pub grading_criteria_id: String,
+    pub grading_criteria_grade_alphabet: String,
+    pub grading_criteria_grade_minimum_score: i32,
 }
 
-pub struct OpeningSubjectsInSemesterSubjectCapacities {
-    pub semester_id: String,
-    pub subject_id: String,
-    pub subject_capacity: i32,
+#[derive(ger_from_row::FromRow)]
+pub struct GradingCriterias {
+    pub grading_criteria_id: String,
+    pub user_id: String,
+    pub grading_criteria_name: String,
+    pub grading_criteria_description: String,
+    pub grading_created_timestamp: time::OffsetDateTime,
 }
 
-pub struct OpeningSubjectsInSemesterAdditionalEligibleStudents {
-    pub semester_id: String,
-    pub subject_id: String,
-    pub additional_student_id: String,
-}
-
-pub struct StudentScores {
-    pub semester_id: String,
-    pub subject_id: String,
-    pub student_id: String,
-    pub assignment_id: String,
-    pub student_score: f64,
-}
-
+#[derive(ger_from_row::FromRow)]
 pub struct ProfessorNames {
     pub professor_name_id: String,
     pub professor_id: String,
@@ -151,34 +145,113 @@ pub struct ProfessorNames {
     pub professor_last_name: String,
 }
 
-pub struct MajorSubjects {
-    pub major_subject_id: String,
-    pub major_credit_specification_id: String,
-    pub subject_id: String,
-}
-
-pub struct Rooms {
-    pub room_id: String,
-    pub building_id: String,
-    pub building_name: String,
-    pub room_capacity: i32,
-}
-
-pub struct OpeningSubjectsInSemester {
+#[derive(ger_from_row::FromRow)]
+pub struct OpeningSubjectsInSemesterSchedules {
     pub semester_id: String,
     pub subject_schedule_id: String,
     pub room_id: String,
 }
 
+#[derive(ger_from_row::FromRow)]
 pub struct OpeningSubjectsInSemesterProfessors {
     pub semester_id: String,
     pub subject_id: String,
     pub professor_id: String,
 }
 
-pub struct StudentEnrollments {
+#[derive(ger_from_row::FromRow)]
+pub struct OpeningSubjectsInSemesterEligibleMajors {
+    pub semester_id: String,
+    pub subject_id: String,
+    pub major_id: String,
+    pub academic_year_id: String,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct StudentTransactions {
+    pub semester_id: String,
+    pub student_id: String,
+    pub student_transaction_id: String,
+    pub student_transaction_is_transaction_successful: bool,
+    pub student_transaction_price: f64,
+    pub student_transaction_created_timestamp: time::OffsetDateTime,
+    pub student_transaction_finished_timestamp: time::OffsetDateTime,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Faculties {
+    pub faculty_id: String,
+    pub faculty_name: String,
+    pub faculty_created_timestamp: time::OffsetDateTime,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Subjects {
+    pub subject_id: String,
+    pub subject_name: String,
+    pub subject_credit: i32,
+    pub subject_type: String,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct OpeningSubjectsInSemesterAdditionalEligibleStudents {
+    pub semester_id: String,
+    pub subject_id: String,
+    pub additional_student_id: String,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct StudentScores {
     pub semester_id: String,
     pub subject_id: String,
     pub student_id: String,
+    pub assignment_id: String,
+    pub student_score: f64,
 }
 
+#[derive(ger_from_row::FromRow)]
+pub struct Majors {
+    pub major_id: String,
+    pub curriculum_id: String,
+    pub major_name: String,
+    pub major_created_timestamp: time::OffsetDateTime,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct AcademicYears {
+    pub academic_year_id: String,
+    pub academic_year_gregorian_year: i32,
+    pub academic_year_start_timestamp: time::OffsetDateTime,
+    pub academic_year_end_timestamp: time::OffsetDateTime,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Users {
+    pub user_id: String,
+    pub user_username: String,
+    pub user_email: String,
+    pub user_password: String,
+    #[fromrow(num)]
+    pub user_role: Role,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct Students {
+    pub student_id: String,
+    pub student_representative_id: String,
+    pub student_profile_image_path: String,
+    pub student_nid: String,
+    pub student_previous_school_name: String,
+    pub student_previous_school_gpa: f64,
+    pub major_id: String,
+    pub professor_id: String,
+    pub first_academic_year_id: String,
+}
+
+#[derive(ger_from_row::FromRow)]
+pub struct MajorCreditSpecifications {
+    pub major_credit_specification_id: String,
+    pub major_id: String,
+    pub major_credit_specification_name: String,
+    pub major_credit_specification_minimum_credit: i32,
+}
