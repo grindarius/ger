518be0e948efc752705d3bad4c435fb374a35d76
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-01-16 00:19:59 +0700

    add simple page managements

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,7 +1,23 @@
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react'
+
+import Menu from '@/components/menu'
 
 import '@/styles/globals.css'
 
-export default function App ({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App ({ Component, pageProps }: AppPropsWithLayout): ReactNode {
+  // Use the layout defined at the page level, if available
+  // if not, use menu page
+  const getLayout = Component.getLayout ?? ((page) => <Menu>{page}</Menu>)
+
+  return getLayout(<Component {...pageProps} />)
 }
---
+++ packages/frontend/src/pages/signin/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Signin (): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Signin • ger</title>
+        <meta name="description" content="reg spelled backwards" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <h1 className="text-4xl">Signin hereeee</h1>
+    </>
+  )
+}
