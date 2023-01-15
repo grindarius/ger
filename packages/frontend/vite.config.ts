46eb9c3348c6506f7d17c8bfbad06be7389feb51
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-01-15 16:29:14 +0700

    add ssr support, tailwind, and remove most pages

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -6,6 +6,7 @@ import { qwikCity } from '@builder.io/qwik-city/vite'
 
 export default defineConfig(() => {
   return {
+    // TODO: qwikCity({ trailingSlash: true }) will be available in qwikcity 0.1
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
