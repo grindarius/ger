86c2ca59e18317c8e871d56688691cb1dc719e00
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-01-17 18:32:24 +0700

    add simple signin ui

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -14,16 +14,14 @@ const Home: NextPageWithLayout = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content text-center">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-              </p>
-              <Link className="btn btn-primary" href="/signin">Signin</Link>
-            </div>
+      <main className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <Link className="btn btn-primary" href="/signin">Signin</Link>
           </div>
         </div>
       </main>
--- packages/frontend/src/pages/signin/index.tsx
+++ packages/frontend/src/pages/signin/index.tsx
@@ -9,7 +9,35 @@ export default function Signin (): JSX.Element {
         <meta name="description" content="reg spelled backwards" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <h1 className="text-4xl">Signin hereeee</h1>
+      <main className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Signin</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Username or email</span>
+                </label>
+                <input type="text" placeholder="email" className="input input-bordered" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="text" placeholder="password" className="input input-bordered" />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
