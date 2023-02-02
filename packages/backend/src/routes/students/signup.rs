7c27b1f23f2466355f11fb2ddcaa7747d91c5525
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-02-02 10:52:56 +0700

    update student signup branch

--- packages/backend/src/routes/students/signup.rs
+++ packages/backend/src/routes/students/signup.rs
@@ -7,10 +7,11 @@ use serde::Deserialize;
 use tokio_postgres::types::Type;
 use utoipa::ToSchema;
 
-use crate::constants::AuthenticationHeaders;
 use crate::constants::{create_argon2_context, DefaultSuccessResponse, ID_LENGTH};
+use crate::constants::{AuthenticationHeaders, AD_BE_YEAR_DIFFERENCE};
 use crate::database::Role;
 use crate::errors::HttpError;
+use crate::extractors::admins::AuthenticatedAdminClaims;
 use crate::shared_app_data::SharedAppData;
 
 #[derive(Deserialize, ToSchema)]
@@ -21,8 +22,9 @@ pub struct StudentSignupBody {
     major_representative_id: String,
     professor_id: String,
     first_academic_year_id: String,
+    /// The academic year that new students have joined in anno domini year
     #[schema(example = json!("2017"))]
-    first_academic_year_gregorian_year: String,
+    first_academic_year: String,
 }
 
 #[derive(Deserialize, ToSchema)]
@@ -43,8 +45,25 @@ pub struct StudentSignupBodyInner {
 
 #[derive(Deserialize, ger_from_row::FromRow)]
 struct LatestStudentIndex {
-    latest_student_index: u32,
+    student_representative_id: String,
 }
+
+// function getRepresentativeId (previousStudents: Array<[Users, Students]>, major: Majors, firstAcadYear: AcademicYears): string {
+//   const firstAcadYearBE = Number(firstAcadYear.academic_year_anno_domini_year) + 543
+//   const studentsInSameMajorAndYear = previousStudents.filter(ps => ps[1].major_id === major.major_id && ps[1].first_academic_year_id === firstAcadYear.academic_year_id)
+//   const latestStudentId = studentsInSameMajorAndYear.sort((a, b) => {
+//     return Number(b[1].student_representative_id.slice(4)) - Number(a[1].student_representative_id.slice(4))
+//   })
+//
+//   const template = `${firstAcadYearBE.toString().substring(2)}${major.major_representative_id}${((Number(latestStudentId?.[0]?.[1].student_representative_id.slice(4) ?? 0) ?? 0) + 1).toString().padStart(4, '0')}`
+//
+//   if (previousStudents.map(p => p[1].student_representative_id).includes(template)) {
+//     const msg = `redundant student representative id found: representative_id: ${template}`
+//     throw new Error(msg)
+//   }
+//
+//   return template
+// }
 
 /// Bulk signup students either given from a csv file or some admission website.
 #[utoipa::path(
@@ -66,6 +85,12 @@ struct LatestStudentIndex {
             example = json!(HttpError::InputValidationError.get_error_struct())
         ),
         (
+            status = 401,
+            description = "unauthorized error",
+            body = FormattedErrorResponse,
+            example = json!(HttpError::Unauthorized.get_error_struct())
+        ),
+        (
             status = 500,
             description = "internal server errors",
             body = FormattedErrorResponse,
@@ -76,17 +101,19 @@ struct LatestStudentIndex {
 pub async fn handler(
     body: web::Json<StudentSignupBody>,
     data: web::Data<SharedAppData>,
+    _claims: AuthenticatedAdminClaims,
 ) -> Result<HttpResponse, HttpError> {
     if body.students.len() == 0 {
         return Err(HttpError::NoData);
     }
 
     let mut client = data.pool.get().await?;
-    let first_academic_year_anno_domini_year = body
-        .first_academic_year_gregorian_year
+
+    let first_academic_year_bhuddist_era_year = body
+        .first_academic_year
         .parse::<u32>()
         .map_err(|_| HttpError::InputValidationError)?
-        + 543u32;
+        + AD_BE_YEAR_DIFFERENCE;
 
     let insert_user_statement = client
         .prepare_typed_cached(
@@ -105,6 +132,44 @@ pub async fn handler(
                 $5
             )"##,
             &[Type::TEXT, Type::TEXT, Type::TEXT, Type::TEXT, Type::TEXT],
+        )
+        .await?;
+
+    let insert_student_statement = client
+        .prepare_typed_cached(
+            r##"
+            insert into students (
+                student_id,
+                student_representative_id,
+                student_nid,
+                student_birthdate,
+                student_previous_school_name,
+                student_previous_school_gpa,
+                major_id,
+                professor_id,
+                first_academic_year_id
+            ) values (
+                $1,
+                $2,
+                $3,
+                $4,
+                $5,
+                $6,
+                $7,
+                $8,
+                $9
+            )"##,
+            &[
+                Type::TEXT,
+                Type::TEXT,
+                Type::TEXT,
+                Type::DATE,
+                Type::TEXT,
+                Type::NUMERIC,
+                Type::TEXT,
+                Type::TEXT,
+                Type::TEXT,
+            ],
         )
         .await?;
 
@@ -137,59 +202,25 @@ pub async fn handler(
         )
         .await?;
 
-    let insert_student_statement = client
-        .prepare_typed_cached(
-            r##"insert into students (
-                    student_id,
-                    student_representative_id,
-                    student_nid,
-                    student_birthdate,
-                    student_previous_school_name,
-                    student_previous_school_gpa,
-                    major_id,
-                    professor_id,
-                    first_academic_year_id
-                ) values (
-                    $1,
-                    $2,
-                    $3,
-                    $4,
-                    $5,
-                    $6,
-                    $7,
-                    $8,
-                    $9
-                )"##,
-            &[
-                Type::TEXT,
-                Type::TEXT,
-                Type::TEXT,
-                Type::DATE,
-                Type::TEXT,
-                Type::NUMERIC,
-                Type::TEXT,
-                Type::TEXT,
-                Type::TEXT,
-            ],
-        )
-        .await?;
-
     let argon2_context = create_argon2_context()?;
 
     let latest_student_index = client
         .query_one(
             r##"
             select
-                count(student_id) as latest_student_index 
+                student_representative_id
             from students
             where major_id = $1 and first_academic_year_id = $2"##,
             &[&body.major_id, &body.first_academic_year_id],
         )
         .await?;
     let latest_student_index = LatestStudentIndex::try_from(latest_student_index)?;
+    let latest_student_number = latest_student_index.student_representative_id[4..]
+        .parse::<usize>()
+        .unwrap_or(0usize);
 
     for (i, student) in body.students.iter().enumerate() {
-        let transaction = client.transaction().await.unwrap();
+        let transaction = client.transaction().await?;
 
         let new_student_id = nanoid::nanoid!(ID_LENGTH);
         let new_student_email = format!(
@@ -202,18 +233,22 @@ pub async fn handler(
                 .nth(0)
                 .unwrap_or('s')
                 .to_string(),
-            &first_academic_year_anno_domini_year,
+            &first_academic_year_bhuddist_era_year,
         );
-        let new_student_major_index = latest_student_index.latest_student_index as usize + i;
+        let new_student_index = latest_student_number + i;
+
+        if new_student_index == 10000 {
+            tracing::error!("new student_index overflow to {}", new_student_index);
+            return Err(HttpError::InternalServerError);
+        }
+
         let new_student_representative_id = format!(
             "{}{}{:0>4}",
-            first_academic_year_anno_domini_year,
-            body.major_representative_id,
-            new_student_major_index
+            first_academic_year_bhuddist_era_year, body.major_representative_id, new_student_index
         );
         let salt = SaltString::generate(&mut OsRng);
-        let new_student_account_password = create_argon2_context()?
-            .hash_password(&new_student_representative_id.as_bytes(), &salt)?;
+        let new_student_account_password =
+            argon2_context.hash_password(&new_student_representative_id.as_bytes(), &salt)?;
 
         transaction
             .execute(
@@ -227,11 +262,34 @@ pub async fn handler(
                 ],
             )
             .await?;
-
-        transaction.execute(&insert_student_statement, &[]).await?;
-
         transaction
-            .execute(&insert_student_name_statement, &[])
+            .execute(
+                &insert_student_statement,
+                &[
+                    &new_student_id,
+                    &new_student_representative_id,
+                    &student.student_nid,
+                    &student.student_birthdate,
+                    &student.student_previous_school_name,
+                    &student.student_previous_school_gpa,
+                    &body.major_id,
+                    &body.professor_id,
+                    &body.first_academic_year_id,
+                ],
+            )
+            .await?;
+        transaction
+            .execute(
+                &insert_student_name_statement,
+                &[
+                    &nanoid::nanoid!(ID_LENGTH),
+                    &new_student_id,
+                    &"EN",
+                    &student.student_english_first_name.trim(),
+                    &student.student_english_middle_name.trim(),
+                    &student.student_english_last_name.trim(),
+                ],
+            )
             .await?;
 
         transaction.commit().await?;
