63ff99871ee1481f1b2794f5421ffbcf0aa6de4b
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-02-05 23:26:17 +0700

    templating code

--- packages/backend/src/routes/forum/get_trending_posts_list.rs
+++ packages/backend/src/routes/forum/get_trending_posts_list.rs
@@ -4,29 +4,39 @@ use actix_web::{web, HttpResponse};
 use serde::{de, Deserialize, Deserializer};
 use utoipa::{IntoParams, ToSchema};
 
-use crate::{errors::HttpError, extractors::users::AuthenticatedUserClaims};
+use crate::{
+    constants::AuthenticationHeaders, errors::HttpError, extractors::users::AuthenticatedUserClaims,
+};
 
 #[derive(Deserialize, ToSchema, IntoParams)]
 #[into_params(style = Form, parameter_in = Query)]
 pub struct GetTrendingPostsListRequestQueries {
     /// How big of a window to check for the trending posts. like "trending in the last 24
-    /// hours"
+    /// hours". default is 24
     #[serde(default, deserialize_with = "empty_string_as_none")]
     pub hours: Option<u32>,
-    /// How much of a post to query for.
+    /// How much of a post to query for. default is 10
     #[serde(default, deserialize_with = "empty_string_as_none")]
     pub limit: Option<u32>,
-    /// How much of a post to skip as a page change.
+    /// How much of a post to skip as a page change. default is 0
     #[serde(default, deserialize_with = "empty_string_as_none")]
-    pub skip: Option<u32>,
+    pub offset: Option<u32>,
 }
 
 /// Gets trending posts list with a given limit and a time window.
-#[utoipa::path(get, path = "/forum/trending")]
+#[utoipa::path(
+    get,
+    path = "/forum/trending",
+    params(AuthenticationHeaders, GetTrendingPostsListRequestQueries)
+)]
 pub async fn handler(
     query: web::Query<GetTrendingPostsListRequestQueries>,
     _claims: AuthenticatedUserClaims,
 ) -> Result<HttpResponse, HttpError> {
+    let hours = query.hours.unwrap_or(24);
+    let limit = query.limit.unwrap_or(10);
+    let offset = query.offset.unwrap_or(0);
+
     Ok(HttpResponse::Ok().finish())
 }
 
@@ -45,5 +55,27 @@ where
     match opt.as_deref() {
         None | Some("") => Ok(None),
         Some(s) => FromStr::from_str(s).map_err(de::Error::custom).map(Some),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{http::header::ContentType, test, web::Query};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_query_deserializer() {
+        let request = test::TestRequest::default()
+            .insert_header(ContentType::json())
+            .to_http_request();
+
+        let query = Query(GetTrendingPostsListRequestQueries {
+            hours: 24,
+            limit: 10,
+            offset: 0,
+        });
+
+        let response = handler(query, claims).await;
     }
 }
