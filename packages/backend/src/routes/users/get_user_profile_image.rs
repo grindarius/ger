606e2728bdef9b81a9467f386a6399f526b15481
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-02-10 14:21:46 +0700

    new routes

---
+++ packages/backend/src/routes/users/get_user_profile_image.rs
@@ -0,0 +1,40 @@
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
+use utoipa::IntoParams;
+
+use crate::{
+    constants::swagger::AuthenticationHeaders, errors::HttpError,
+    extractors::users::AuthenticatedUserClaims,
+};
+
+#[derive(Deserialize, IntoParams)]
+#[into_params(parameter_in = Path)]
+pub struct GetUserProfileImageParams {
+    user_id: String,
+}
+
+#[utoipa::path(
+    get,
+    path = "/users/{user_id}/profile-image",
+    tag = "users",
+    operation_id = "get_user_profile_image",
+    params(AuthenticationHeaders, GetUserProfileImageParams)
+)]
+pub async fn handler(
+    path: web::Path<GetUserProfileImageParams>,
+    _claims: AuthenticatedUserClaims,
+) -> Result<HttpResponse, HttpError> {
+    Ok(HttpResponse::Ok().finish())
+}
+
+/// Generates a unique pastel color from a given username
+fn username_to_hsl_color(name: &str) -> u32 {
+    let mut hash: u32 = 0;
+
+    for c in name.chars() {
+        hash = (c as u32) + ((hash << 5) - hash);
+    }
+
+    let hash = hash % 360;
+    hash
+}
