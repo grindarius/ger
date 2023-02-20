8d1cebb517af6ab8456911b56e18638eeaf54fe2
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-02-20 17:15:09 +0700

    using new logic to fetch more data client side

--- packages/frontend/src/pages/forum/index.tsx
+++ packages/frontend/src/pages/forum/index.tsx
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs'
-import got from 'got'
 import type { GetServerSidePropsResult } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 
 import type { GetPostListRequestQueries } from '@/types/GetPostListRequestQueries'
 import type { GetPostListResponseBody } from '@/types/GetPostListResponseBody'
@@ -15,15 +15,16 @@ export async function getServerSideProps (): Promise<GetServerSidePropsResult<{ 
   }
 
   try {
+    const url = new URL('http://127.0.0.1:5155/forum/posts')
     const searchParams = new URLSearchParams(queries as Record<string, string>)
+    url.search = searchParams.toString()
 
-    const response = await got.get('http://127.0.0.1:5155/forum/posts', {
-      searchParams
-    }).json<GetPostListResponseBody>()
+    const response = await fetch(url)
+    const announcements: GetPostListResponseBody = await response.json()
 
     return {
       props: {
-        announcements: response
+        announcements
       }
     }
   } catch (e) {
@@ -40,6 +41,51 @@ export async function getServerSideProps (): Promise<GetServerSidePropsResult<{ 
 }
 
 function Forum ({ announcements }: { announcements: GetPostListResponseBody }): JSX.Element {
+  const [page, setPage] = useState(1)
+  const [pageAnnouncements, setPageAnnouncements] = useState(announcements)
+
+  const fetchAnnouncements = async (nextPage: number): Promise<void> => {
+    const queries: GetPostListRequestQueries = {
+      announcement: true,
+      category_based_announcement: false,
+      page: nextPage
+    }
+
+    try {
+      const url = new URL('http://127.0.0.1:5155/forum/posts')
+      const searchParams = new URLSearchParams(queries as Record<string, string>)
+      url.search = searchParams.toString()
+
+      const response = await fetch(url)
+      const announcements: GetPostListResponseBody = await response.json()
+
+      setPageAnnouncements(announcements)
+    } catch (e) {
+      const response: GetPostListResponseBody = {
+        posts: []
+      }
+
+      setPageAnnouncements(response)
+    }
+  }
+
+  async function goToPreviousPage (): Promise<void> {
+    const nextPage = page - 1
+
+    if (nextPage <= 0) {
+      return
+    }
+
+    setPage(nextPage)
+    await fetchAnnouncements(nextPage)
+  }
+
+  async function goToNextPage (): Promise<void> {
+    const nextPage = page + 1
+    setPage(nextPage)
+    await fetchAnnouncements(nextPage)
+  }
+
   return (
     <>
       <Head>
@@ -48,9 +94,14 @@ function Forum ({ announcements }: { announcements: GetPostListResponseBody }): 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="container mx-auto">
-        <h1 className="text-4xl text-current font-bold">Forum</h1>
-
-        <h3 className="text-2xl text-current">Global announcements</h3>
+        <h1 className="text-4xl text-current font-bold">Forum {page}</h1>
+        <div className="flex flex-row justify-between">
+          <h3 className="text-2xl text-current">Global announcements</h3>
+          <div className="flex flex-row btn-group">
+            <button className="btn" onClick={goToPreviousPage}>Previous page</button>
+            <button className="btn" onClick={goToNextPage}>Next page</button>
+          </div>
+        </div>
         <div className="overflow-x-auto">
           <table className="table w-full">
             <thead>
@@ -63,7 +114,7 @@ function Forum ({ announcements }: { announcements: GetPostListResponseBody }): 
             </thead>
             <tbody>
               {
-                announcements.posts.map(a => {
+                pageAnnouncements.posts.map(a => {
                   return (
                     <tr key={a.id}>
                       <td>
