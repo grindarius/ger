be5e4404fe30364e0cce9b1123466775b1876afc
Bhattarpong Somwong <numbbutt34685@gmail.com>
2023-02-21 10:52:19 +0700

    new function to update post's last active timestamp

--- packages/faker/src/tables/forums/forum-post-replies.ts
+++ packages/faker/src/tables/forums/forum-post-replies.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { nanoid } from 'nanoid'
 
 import { faker } from '@faker-js/faker'
@@ -5,20 +6,43 @@ import { faker } from '@faker-js/faker'
 import type { ForumPostReplies, ForumPosts, Users } from '../../database.js'
 import { NANOID_LENGTH } from '../../generals.js'
 
-export function generateForumPostReplies (users: Array<Users>, posts: Array<ForumPosts>): Array<ForumPostReplies> {
+export function generateForumPostReplies (users: Array<Users>, posts: Array<ForumPosts>): [Array<ForumPosts>, Array<ForumPostReplies>] {
   const postReplies = posts.map(p => {
+    const replyCreatedTimestamp = faker.date.between(p.forum_post_created_timestamp, new Date()).toISOString()
+
     return Array.from({ length: faker.datatype.number({ min: 0, max: 10 }) }, () => {
       const reply: ForumPostReplies = {
         forum_post_reply_id: nanoid(NANOID_LENGTH),
         forum_post_id: p.forum_post_id,
         user_id: faker.helpers.arrayElement(users).user_id,
         forum_post_reply_content: faker.lorem.paragraphs(3).replaceAll(',', ''),
-        forum_post_reply_created_timestamp: faker.date.between(p.forum_post_created_timestamp, new Date()).toISOString()
+        forum_post_reply_created_timestamp: replyCreatedTimestamp
       }
 
       return reply
     })
   }).flat()
 
-  return postReplies
+  const postsWithUpdatedLastActiveTimestamp = posts.map(p => {
+    const allRepliesInAPost = postReplies.filter(pr => {
+      return pr.forum_post_id === p.forum_post_id
+    })
+
+    const latestPostTime = allRepliesInAPost.sort((a, b) => {
+      return dayjs(b.forum_post_reply_created_timestamp).unix() - dayjs(a.forum_post_reply_created_timestamp).unix()
+    })
+
+    if (latestPostTime.length !== 0) {
+      if (latestPostTime[0] == null) {
+        return p
+      }
+
+      const postWithNewLastUpdatedTimestamp: ForumPosts = { ...p, forum_post_last_active_timestamp: latestPostTime[0].forum_post_reply_created_timestamp }
+      return postWithNewLastUpdatedTimestamp
+    }
+
+    return p
+  })
+
+  return [postsWithUpdatedLastActiveTimestamp, postReplies]
 }
