b8aa061a376d9a06a81fefc6452f3bf8ff80d9db
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-02-22 10:39:40 +0700

    complete route

--- packages/backend/src/routes/forum/categories/get_categories_list.rs
+++ packages/backend/src/routes/forum/categories/get_categories_list.rs
@@ -1,10 +1,17 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+use ger_from_row::FromRow;
+use postgres_types::Type;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use ts_rs::TS;
 use utoipa::{IntoParams, ToSchema};
 
-use crate::{constants::DEFAULT_PAGE, constants::DEFAULT_PAGE_SIZE, errors::HttpError};
+use crate::{
+    constants::DEFAULT_PAGE,
+    constants::{SqlRange, DEFAULT_PAGE_SIZE},
+    errors::HttpError,
+    shared_app_data::SharedAppData,
+};
 
 #[derive(Deserialize, ToSchema, IntoParams, TS)]
 #[into_params(parameter_in = Query)]
@@ -26,15 +33,18 @@ pub struct GetCategoriesListResponseBody {
     categories: Vec<GetCategoriesListResponseBodyInner>,
 }
 
-#[derive(Serialize, ToSchema, TS)]
+#[derive(Serialize, ToSchema, TS, FromRow)]
 #[ts(export)]
 pub struct GetCategoriesListResponseBodyInner {
     id: String,
     representative_id: String,
-    posts_count: i64,
+    name: String,
+    #[ts(type = "number")]
+    post_count: i64,
     latest_post_id: String,
     latest_post_name: String,
     latest_post_user_id: String,
+    latest_post_username: String,
     #[serde(with = "time::serde::rfc3339")]
     #[ts(type = "string")]
     latest_post_created_timestamp: time::OffsetDateTime,
@@ -55,6 +65,75 @@ pub struct GetCategoriesListResponseBodyInner {
         ),
     )
 )]
-pub async fn handler() -> Result<HttpResponse, HttpError> {
-    Ok(HttpResponse::Ok().json(json!({ "categories": [] })))
+pub async fn handler(
+    query: web::Query<GetCategoriesListRequestQueries>,
+    data: web::Data<SharedAppData>,
+) -> Result<HttpResponse, HttpError> {
+    let page = query.page.unwrap_or(DEFAULT_PAGE);
+    let page_size = query.page_size.unwrap_or(DEFAULT_PAGE_SIZE);
+    let SqlRange { limit, offset } = SqlRange::from_page(page, page_size)?;
+
+    let client = data.pool.get().await?;
+
+    let statement = client
+        .prepare_typed_cached(
+            r##"
+            with first_row as (
+	            select
+                    rank() over (
+                        partition by
+                            forum_posts.forum_category_id
+                        order by
+                            forum_posts.forum_post_created_timestamp desc
+                    ) as created_rank,
+                    forum_posts.forum_category_id as id,
+                    forum_categories.forum_category_representative_id as representative_id,
+                    forum_categories.forum_category_name as name,
+                    forum_posts.forum_post_id as latest_post_id,
+                    forum_posts.forum_post_name as latest_post_name,
+                    forum_posts.forum_post_created_timestamp as latest_post_created_timestamp,
+                    forum_posts.user_id as latest_post_user_id,
+                    users.user_username as latest_post_username
+                from forum_posts
+                inner join forum_categories on forum_posts.forum_category_id = forum_categories.forum_category_id
+                inner join users on forum_posts.user_id = users.user_id
+            )
+            select
+                created_rank,
+                id,
+                representative_id,
+                name,
+                latest_post_id,
+                latest_post_name,
+                latest_post_created_timestamp,
+                latest_post_user_id,
+                latest_post_username,
+                count(forum_posts.forum_post_id) as post_count
+            from first_row
+            inner join forum_posts on id = forum_posts.forum_category_id
+            where created_rank = 1
+            group by
+                first_row.created_rank,
+                first_row.id,
+                first_row.representative_id,
+                first_row.name,
+                first_row.latest_post_id,
+                first_row.latest_post_name,
+                first_row.latest_post_created_timestamp,
+                first_row.latest_post_user_id,
+                first_row.latest_post_username
+            limit $1
+            offset $2
+            "##,
+            &[Type::INT4, Type::INT4],
+        )
+        .await?;
+
+    let categories = client.query(&statement, &[&limit, &offset]).await?;
+    let categories = categories
+        .iter()
+        .map(|c| GetCategoriesListResponseBodyInner::try_from(c))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(HttpResponse::Ok().json(GetCategoriesListResponseBody { categories }))
 }
