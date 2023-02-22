eadc891565c5e7e40472211478c9991790b078f7
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-02-22 11:39:49 +0700

    use shelljs

--- packages/scripts/move-bindings.mjs
+++ packages/scripts/move-bindings.mjs
@@ -1,15 +1,4 @@
-import { exec } from 'node:child_process'
-import { fileURLToPath } from 'node:url'
-import { dirname } from 'node:path'
+import shell from 'shelljs'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-exec('cp ../backend/bindings/* ../frontend/src/types/', { cwd: __dirname }, function (error, stdout, stderr) {
-  console.log(stdout)
-  console.log(stderr)
-
-  if (error != null) {
-    throw error
-  }
-})
+shell.echo(process.cwd())
+shell.cp('-r', '../backend/bindings/*', '../frontend/src/types/')
