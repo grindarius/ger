b5ae9fdc3ea5f9a4810ba628f93fed2cb2d847b6
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-03-01 23:36:06 +0700

    return new row of the database for frontend

--- packages/faker/src/tables/forums/forum-categories.ts
+++ packages/faker/src/tables/forums/forum-categories.ts
@@ -15,6 +15,7 @@ export function generateForumCategories (users: Array<Users>): Array<ForumCatego
       forum_category_id: nanoid(NANOID_LENGTH),
       forum_category_name: 'Help',
       forum_category_representative_id: slug('Help'),
+      forum_category_description: 'Get help from your lovely friend.',
       user_id: faker.helpers.arrayElement(admins).user_id,
       forum_category_color_theme: faker.color.rgb({ prefix: '#' }),
       forum_category_created_timestamp: faker.date.past(10).toISOString()
@@ -23,6 +24,7 @@ export function generateForumCategories (users: Array<Users>): Array<ForumCatego
       forum_category_id: nanoid(NANOID_LENGTH),
       forum_category_name: 'Advertisements',
       forum_category_representative_id: 'advertisements',
+      forum_category_description: 'For anyone looking to sell stuffs and all that.',
       user_id: faker.helpers.arrayElement(admins).user_id,
       forum_category_color_theme: faker.color.rgb({ prefix: '#' }),
       forum_category_created_timestamp: faker.date.past(10).toISOString()
@@ -31,6 +33,7 @@ export function generateForumCategories (users: Array<Users>): Array<ForumCatego
       forum_category_id: nanoid(NANOID_LENGTH),
       forum_category_name: 'Uncategorized',
       forum_category_representative_id: 'uncategorized',
+      forum_category_description: 'For anyone that haven\'t figured out yet about where this thing goes.',
       user_id: faker.helpers.arrayElement(admins).user_id,
       forum_category_color_theme: faker.color.rgb({ prefix: '#' }),
       forum_category_created_timestamp: faker.date.past(10).toISOString()
@@ -39,6 +42,7 @@ export function generateForumCategories (users: Array<Users>): Array<ForumCatego
       forum_category_id: nanoid(NANOID_LENGTH),
       forum_category_name: 'Memes',
       forum_category_representative_id: 'memes',
+      forum_category_description: 'meme related content',
       user_id: faker.helpers.arrayElement(admins).user_id,
       forum_category_color_theme: faker.color.rgb({ prefix: '#' }),
       forum_category_created_timestamp: faker.date.past(10).toISOString()
@@ -47,6 +51,7 @@ export function generateForumCategories (users: Array<Users>): Array<ForumCatego
       forum_category_id: nanoid(NANOID_LENGTH),
       forum_category_name: 'Homeworks',
       forum_category_representative_id: slug('Homeworks'),
+      forum_category_description: 'Section for the hopeless',
       user_id: faker.helpers.arrayElement(admins).user_id,
       forum_category_color_theme: faker.color.rgb({ prefix: '#' }),
       forum_category_created_timestamp: faker.date.past(10).toISOString()
